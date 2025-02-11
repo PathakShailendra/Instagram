2146f02f6e81482525da975ee36e269c12c78259
Shailendra pathak <pathakharsh813@gmail.com>
2025-02-11 11:17:04 +0530

    like and dislike of post completed

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,10 +1,133 @@
 import { generateCaptionFromImageBuffer } from "../services/ai.service.js";
 import { uploadFile } from "../services/cloudStorage.service.js";
+import postModel from "../models/post.model.js";
+import likeModel from "../models/likes.model.js";
 
 
 
 export const createPost = async (req, res, next) => {
-  const imageBuffer = req.file.buffer;
-  const filedata = await uploadFile(imageBuffer);
-  res.status(201).json({ filedata });
-};
+
+  try {
+
+
+      const imageBuffer = req.file?.buffer;
+
+      if (!imageBuffer) {
+          return res.status(400).json({ message: "Image is required" });
+      }
+
+      const [ caption, fileData ] = await Promise.all([
+          generateCaptionFromImageBuffer(imageBuffer),
+          uploadFile(imageBuffer)
+      ]);
+
+      const newPost = await postModel.create({
+          caption,
+          media: fileData,
+          author: req.user._id
+      });
+
+
+      res.status(201).json({
+          post: newPost,
+          message: "Post created successfully"
+      });
+
+  } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+  }
+
+
+
+}
+
+
+export const likePost = async (req, res, next) => {
+
+  try {
+
+      const postId = req.params.postId;
+
+      if (!postModel.isValidPostId(postId)) {
+          return res.status(400).json({ message: "Invalid Post ID" });
+      }
+
+      const post = await postModel.findById(postId);
+
+      if (!post) {
+          return res.status(404).json({ message: "Post not found" });
+      }
+
+      const isAlreadyLiked = await likeModel.findOne({
+          post: postId,
+          user: req.user._id
+      })
+
+      if (isAlreadyLiked) {
+          return res.status(200).json({ message: "Post already liked" });
+      }
+
+      await likeModel.create({
+          post: postId,
+          user: req.user._id
+      });
+
+      await post.incrementLikeCount();
+
+      res.status(200).json({ message: "Post liked" });
+
+  } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+  }
+}
+
+
+
+export const removeLikePost = async (req, res, next) => {
+
+  try {
+
+      const postId = req.params.postId;
+
+      if (!postModel.isValidPostId(postId)) {
+          return res.status(400).json({ message: "Invalid Post ID" });
+      }
+
+      const post = await postModel.findById(postId);
+
+      if (!post) {
+          return res.status(404).json({ message: "Post not found" });
+      }
+
+
+      const userLikedPost = await likeModel.findOne({
+          post: postId,
+          user: req.user._id
+      })
+
+
+      if (!userLikedPost) {
+          return res.status(200).json({ message: "Post not liked" });
+      }
+
+      await likeModel.findOneAndDelete({
+          post: postId,
+          user: req.user._id
+      })
+
+      await post.decrementLikeCount();
+
+      res.status(200).json({ message: "Post unliked" });
+
+
+
+  } catch (err) {
+
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+
+  }
+
+}
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import * as postController from "../controllers/post.controller.js";
 import * as postMiddleware from "../middlewares/post.middleware.js";
+import * as userMiddleware from "../middlewares/user.middleware.js";
 const router = Router();
 
-router.post('/create', postMiddleware.handleFileUpload ,postController.createPost)
+router.post("/create",
+    userMiddleware.authUser,
+    postMiddleware.handleFileUpload,
+    postController.createPost
+)
 
+router.patch("/like/:postId",
+    userMiddleware.authUser,
+    postController.likePost
+)
 
-
+router.patch("/remove-like/:postId",
+    userMiddleware.authUser,
+    postController.removeLikePost
+)
 
 export default router;
