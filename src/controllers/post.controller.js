3d2060d09692114970aa3482a8e148606c9b8bd6
Shailendra pathak <pathakharsh813@gmail.com>
2025-02-11 09:45:24 +0530

    file uploaded with cloudinary

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,9 +1,10 @@
 import { generateCaptionFromImageBuffer } from "../services/ai.service.js";
+import { uploadFile } from "../services/cloudStorage.service.js";
 
 
 
 export const createPost = async (req, res, next) => {
   const imageBuffer = req.file.buffer;
-  const caption = await generateCaptionFromImageBuffer(imageBuffer);
-  res.status(201).json({ caption });
+  const filedata = await uploadFile(imageBuffer);
+  res.status(201).json({ filedata });
 };
